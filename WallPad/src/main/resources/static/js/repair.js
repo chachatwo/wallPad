f9f674dfa171c7496cb69cf5d81298fbbc796e46
chachatwo <dnjscjf0104@naver.com>
2025-03-18 11:50:33 +0900

    Add repair.js file

---
+++ WallPad/src/main/resources/static/js/repair.js
@@ -0,0 +1,13 @@
+function validateForm() {
+	const apartmentNumber = document.getElementById('apartmentNumber').value;
+	const apartmentNum = /^\d{3}동\d{3,4}호$/;
+	const errorMessage = document.getElementById('error-message');
+
+	if (!apartmentNum.test(apartmentNumber)) {
+		errorMessage.style.display = 'block';
+		return false; 
+	}
+
+	errorMessage.style.display = 'none';
+	return true;
+}
